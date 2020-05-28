26aaecb0137da27f40564c5d2ee48ef79754702e
Konstantinos Houtas <reyd0905@gmail.com>
2020-05-27 22:22:47 -0400

    Another slight modification

--- queries/sql/sql_getAllSongs.sql
+++ queries/sql/sql_getAllSongs.sql
@@ -16,7 +16,7 @@ FROM
 INNER JOIN users u ON s.user_fk = u.id
 INNER JOIN charts c ON c.song_fk = s.id
 INNER JOIN (
-    SELECT song_fk FROM charts WHERE charts.level > ${lower} AND charts.level < ${upper}
+    SELECT DISTINCT song_fk FROM charts WHERE charts.level > ${lower} AND charts.level < ${upper}
 ) levelCheck ON levelCheck.song_fk = s.id
 WHERE (${search}) AND game ILIKE ${game} AND type ILIKE ${type}
 GROUP BY s.id, u.id ORDER BY id DESC OFFSET ${offset} LIMIT 20;
