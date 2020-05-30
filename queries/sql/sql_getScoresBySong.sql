057ab0cc3fee08273ff6fd86f142211195e28c79
Konstantinos Houtas <reyd0905@gmail.com>
2020-05-29 20:32:27 -0400

    Fixed scores not displaying for specific user (displayed all instead)

--- queries/sql/sql_getScoresBySong.sql
+++ queries/sql/sql_getScoresBySong.sql
@@ -1,22 +1,18 @@
 SElECT 
 	s.*,
-	json_agg(c) AS difficulties 
+	json_agg(sub2) AS difficulties 
 FROM 
 	songs s 
 INNER JOIN (
-	SELECT c.*, json_agg(sc.* ORDER BY score desc) AS scores FROM charts c
+	SELECT c.*, json_agg(sub) scores FROM charts c
 	LEFT OUTER JOIN (
 		SELECT sc.*, ct.type FROM scores sc
 		INNER JOIN clear_types ct ON ct.id = sc.clear_fk
-	) sc ON sc.chart_fk = c.id
-	LEFT OUTER JOIN (
-		SELECT DISTINCT chart_fk FROM scores 
-		LEFT OUTER JOIN clear_types ct ON ct.id = scores.clear_fk
-		WHERE scores.user_fk = ${userID}
-	) userCheck ON userCheck.chart_fk = c.id
+		WHERE user_fk = ${userID}
+		ORDER BY score DESC
+	) sub ON sub.chart_fk = c.id
 	GROUP BY c.id
-) c ON c.song_fk = s.id
+) sub2 ON sub2.song_fk = s.id
 WHERE 
 	s.id = ${songID}
-GROUP BY 
-	s.id;
+GROUP BY s.id
